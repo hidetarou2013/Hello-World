037ccb6cfa5138e7bd1eea9347b2e0d456577b6f
Hidetoshi Maekawa <hide1227@gmail.com>
2014-06-25 18:36:39 +0900

    test

--- hello_world6.php
+++ hello_world6.php
@@ -1,5 +1,5 @@
 <?php
-    echo "Hello World ...Hide5." 
+    echo "Hello World ...Hide6. modify on eclipse" 
     echo "Hey Hide from github site."
     echo "Hoge Hoge from local repo master."
 ?>
