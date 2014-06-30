d0906ee576786103e04e6fe290abe0da44ffeee2
Hidetoshi Maekawa <hide1227@gmail.com>
2014-06-30 09:43:23 +0900

    from IPTV-PC

--- hello_world5.php
+++ hello_world5.php
@@ -1,5 +1,5 @@
 <?php
-    echo "Hello World ...Hide5." 
+    echo "Hello World ...Hide5.from eclipse on IPTV-PC" 
     echo "Hey Hide from github site."
     echo "Hoge Hoge from local repo master."
 ?>
