56fcc065ba304d4f5380d5635f408e081e9e8e70
Hidetoshi Maekawa <hide1227@gmail.com>
2014-06-30 11:35:21 +0900

    modify on eclipse vm 55.122

--- hello_world6.php
+++ hello_world6.php
@@ -1,5 +1,5 @@
 <?php
-    echo "Hello World ...Hide6. modify on eclipse" 
+    echo "Hello World ...Hide6. modify on eclipse vm 55.122"
     echo "Hey Hide from github site."
     echo "Hoge Hoge from local repo master."
 ?>
